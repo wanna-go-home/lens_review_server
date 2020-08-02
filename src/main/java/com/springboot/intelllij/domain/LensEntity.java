804783238f7bafc1890b6f71cec7578d118a9aba
zamonia500 <zamonia500@gmail.com>
2020-08-02 21:19:45 +0900

    Change column datatype : String to JsonNode

--- src/main/java/com/springboot/intelllij/domain/LensEntity.java
+++ src/main/java/com/springboot/intelllij/domain/LensEntity.java
@@ -1,53 +1,60 @@
 package com.springboot.intelllij.domain;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.TypeDef;
+import springfox.documentation.spring.web.json.Json;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "\"Lens\"")
+@Table(name = "lens")
+@TypeDef(
+        typeClass = JsonBinaryType.class,
+        defaultForType = JsonNode.class
+)
 public class LensEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "\"lensId\"")
+    @Column(name = "lens_id")
     private Integer lensId;
 
-    @Column(name = "\"name\"", nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "\"graphicDia\"", nullable = true)
+    @Column(name = "graphic_dia", nullable = true)
     private Double graphicDia;
 
-    @Column(name = "\"perPackage\"", nullable = true)
+    @Column(name = "per_package", nullable = true)
     private Integer perPackage;
 
-    @Column(name = "\"price\"", nullable = true)
+    @Column(name = "price", nullable = true)
     private Integer price;
 
-    @Column(name = "\"reviewCnt\"", nullable = true)
+    @Column(name = "review_cnt", nullable = true)
     private Integer reviewCnt;
 
-    @Column(name = "\"bc\"", nullable = true)
+    @Column(name = "bc", nullable = true)
     private Double bc;
 
-    @Column(name = "\"dia\"", nullable = true)
+    @Column(name = "dia", nullable = true)
     private Double dia;
 
-    @Column(name = "\"url\"", nullable = true)
+    @Column(name = "url", nullable = true)
     private String url;
 
-    @Column(name = "\"productImage\"", nullable = true)
-    private String productImage;
+    @Column(name = "product_image", columnDefinition = "jsonb", nullable = true)
+    private JsonNode productImage;
 
-    @Column(name = "\"demonstrationImage\"", nullable = true)
-    private String demonstrationImage;
+    @Column(name = "demonstration_image", columnDefinition = "jsonb", nullable = true)
+    private JsonNode demonstrationImage;
 
-    @Column(name = "\"per\"", nullable = true)
-    private String per;
-
+    @Column(name = "per", columnDefinition = "jsonb", nullable = true)
+    private JsonNode per;
 
 }
