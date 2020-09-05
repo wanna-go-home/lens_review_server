8eed0932f9cfc61e7c2b6691812cacc8619c6529
yoowonyoung <wonyoung0105@gmail.com>
2020-09-05 19:24:59 +0900

    change annotation

--- src/main/java/com/springboot/intelllij/controller/UserController.java
+++ src/main/java/com/springboot/intelllij/controller/UserController.java
@@ -6,10 +6,11 @@ import com.springboot.intelllij.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
