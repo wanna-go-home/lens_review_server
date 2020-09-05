0817e158ca9d1982897d985a02a8cb09697261c7
zamonia500 <zamonia500@gmail.com>
2020-09-05 19:34:17 +0900

    Change FreeBoardController annotation Controller -> RestController

--- src/main/java/com/springboot/intelllij/controller/FreeBoardController.java
+++ src/main/java/com/springboot/intelllij/controller/FreeBoardController.java
@@ -5,15 +5,11 @@ import com.springboot.intelllij.services.FreeBoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class FreeBoardController {
 
     @Autowired
