cadab75c6149287a8ac06e0dc424f9356b4463fe
zamonia500 <zamonia500@gmail.com>
2020-10-19 23:15:35 +0900

    Add getById API at each boards. Change API path, it closes #18
    # Conflicts:
    #	src/main/java/com/springboot/intelllij/controller/FreeBoardController.java
    #	src/main/java/com/springboot/intelllij/controller/ReviewBoardController.java

--- src/main/java/com/springboot/intelllij/controller/LensInfoController.java
+++ src/main/java/com/springboot/intelllij/controller/LensInfoController.java
@@ -5,7 +5,9 @@ import com.springboot.intelllij.domain.LensEntity;
 import com.springboot.intelllij.domain.LensPreviewEntity;
 import com.springboot.intelllij.services.LensInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,15 +20,11 @@ public class LensInfoController {
     LensInfoService lensInfoService;
 
     @GetMapping
-    public List<LensEntity> getLensInfo(){
-        return lensInfoService.getAllLensInfo();
-    }
+    public List<LensPreviewEntity> getLensesPreview() { return lensInfoService.getLensesPreview(); }
 
     @GetMapping(params = {"id"})
     public LensEntity getLensInfoById(@RequestParam("id") Integer id) {
         return lensInfoService.getLensInfoById(id);
     }
 
-    @GetMapping(value = RESTPath.LENS_PREVIEW)
-    public List<LensPreviewEntity> getLensesPreview() { return lensInfoService.getLensesPreview(); }
 }
