f0757031d9c5a887aca0cce5362fd374520bca3a
zamonia500 <zamonia500@gmail.com>
2020-11-04 22:20:54 +0900

    Refactoring Entities using @MappedSuperclass annotation. Using inheritance between Entity
    # Conflicts:
    #	src/main/java/com/springboot/intelllij/controller/FreeBoardController.java
    #	src/main/java/com/springboot/intelllij/controller/ReviewBoardController.java

--- src/main/java/com/springboot/intelllij/domain/ReviewBoardViewEntity.java
+++ src/main/java/com/springboot/intelllij/domain/ReviewBoardViewEntity.java
@@ -1,47 +1,19 @@
 package com.springboot.intelllij.domain;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-import java.time.ZonedDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "review_board_view")
-public class ReviewBoardViewEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Integer id;
-
-    @Column(name = "account_id")
-    private String account;
-
+public class ReviewBoardViewEntity extends BoardBaseEntity {
     @Column(name = "name")
     private String nickname;
-
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "content")
-    private String content;
-
-    @Column(name = "view_cnt")
-    private Integer viewCnt = 0;
-
-    @Column(name = "like_cnt")
-    private Integer likeCnt;
-
-    @Column(name = "reply_cnt")
-    private Integer replyCnt;
-
-    @Column(name = "created_at")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
-    private ZonedDateTime createdAt;
 
     @Column(name = "lens_id")
     private Integer lensId;
