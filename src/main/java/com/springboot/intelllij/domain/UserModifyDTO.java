30bdaafe3972c6a212df7002a3a8b276f5c25581
yoowonyoung <wonyoung0105@gmail.com>
2021-01-17 23:44:11 +0900

    [Hotfix] Modify Nickname

---
+++ src/main/java/com/springboot/intelllij/domain/UserModifyDTO.java
@@ -0,0 +1,14 @@
+package com.springboot.intelllij.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserModifyDTO {
+    private String nickName;
+}
