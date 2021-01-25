485f5d469f747d0a547fa0d038cadd21ac2961b3
yoowonyoung <wonyoung0105@gmail.com>
2021-01-25 22:31:34 +0900

    [Hotfix] Fix bundle sort

--- src/main/java/com/springboot/intelllij/services/FreeBoardCommentService.java
+++ src/main/java/com/springboot/intelllij/services/FreeBoardCommentService.java
@@ -73,7 +73,7 @@ public class FreeBoardCommentService {
 
         List<CommentOutputDTO> resultCommentList = new ArrayList<>();
         for(FreeBoardCommentEntity commentEntity : comments) {
-            List<FreeBoardCommentEntity> childCommentList = freeBoardCommentRepo.findByBundleIdAndDepth(commentEntity.getId(), CHILD_COMMENT_DEPTH);
+            List<FreeBoardCommentEntity> childCommentList = freeBoardCommentRepo.findByBundleIdAndDepthOrderByCreatedAtAsc(commentEntity.getId(), CHILD_COMMENT_DEPTH);
             resultCommentList.add(new CommentOutputDTO(commentEntity, user.getNickname()));
 
             if(childCommentList.isEmpty()) continue;
@@ -91,7 +91,7 @@ public class FreeBoardCommentService {
         FreeBoardCommentEntity originalComment = freeBoardCommentRepo.findByPostIdAndDepth(postId,COMMENT_DEPTH)
                 .stream().filter(freeBoardCommentEntity -> freeBoardCommentEntity.getId().equals(commentId)).findFirst()
                 .orElseThrow(() -> new NotFoundException(COMMENT_NOT_FOUND));
-        List<FreeBoardCommentEntity> childCommentList = freeBoardCommentRepo.findByBundleIdAndDepth(originalComment.getId(), CHILD_COMMENT_DEPTH);
+        List<FreeBoardCommentEntity> childCommentList = freeBoardCommentRepo.findByBundleIdAndDepthOrderByCreatedAtAsc(originalComment.getId(), CHILD_COMMENT_DEPTH);
         AccountEntity user = UserUtils.getUserEntity();
 
         List<CommentOutputDTO> resultCommentList = new ArrayList<>();
