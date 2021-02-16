9f6d16551224f57e28be4299e4a8b8c36cf0683d
zamonia500 <zamonia500@gmail.com>
2021-02-16 12:13:41 +0900

    Fix code convention of CheckAvailableDTO pojo

--- src/main/java/com/springboot/intelllij/domain/CheckAvailableDTO.java
+++ src/main/java/com/springboot/intelllij/domain/CheckAvailableDTO.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public class CheckAvailableDTO {
     boolean isAvailable;
-    int check_code;
+    int checkCode;
 
-    public CheckAvailableDTO(boolean isAvailable, int check_code) {
+    public CheckAvailableDTO(boolean isAvailable, int checkCode) {
         this.isAvailable = isAvailable;
-        this.check_code = check_code;
+        this.checkCode = checkCode;
     }
 }
